98de8af137d32edf7dd3e2039a6b64b0da2d9580
Marcel Porta Vallés <marcelpv96@gmail.com>
2018-12-19 11:00:17 +0100

    changed the time to predict in the datasets

--- gen_linear_models.R
+++ gen_linear_models.R
@@ -3,13 +3,12 @@
 
 first_segment <- read.csv(file="segments/5K.csv", header=TRUE, sep=",")
 second_segment <- read.csv(file="segments/10K.csv", header=TRUE, sep=",")
-third_segment <- read.csv(file="segments/10K.csv", header=TRUE, sep=",")
-fourth_segment <- read.csv(file="segments/10K.csv", header=TRUE, sep=",")
-fifth_segment <- read.csv(file="segments/10K.csv", header=TRUE, sep=",")
-sixth_segment <- read.csv(file="segments/10K.csv", header=TRUE, sep=",")
-seventh_segment <- read.csv(file="segments/10K.csv", header=TRUE, sep=",")
-eigth_segment <- read.csv(file="segments/10K.csv", header=TRUE, sep=",")
-ninth_segment <- read.csv(file="segments/10K.csv", header=TRUE, sep=",")
+third_segment <- read.csv(file="segments/15K.csv", header=TRUE, sep=",")
+fourth_segment <- read.csv(file="segments/20K.csv", header=TRUE, sep=",")
+fifth_segment <- read.csv(file="segments/25K.csv", header=TRUE, sep=",")
+sixth_segment <- read.csv(file="segments/30K.csv", header=TRUE, sep=",")
+seventh_segment <- read.csv(file="segments/35K.csv", header=TRUE, sep=",")
+eigth_segment <- read.csv(file="segments/40K.csv", header=TRUE, sep=",")
 
 
 
@@ -17,12 +16,18 @@ ninth_segment <- read.csv(file="segments/10K.csv", header=TRUE, sep=",")
 
 lr_1_Seg=lm(arrival_time ~ temperature + Age + Elevation, data=first_segment)
 summary(lr_1_Seg)
-
 lr_2_Seg=lm(arrival_time ~ temperature + Age + Elevation, data=second_segment)
+summary(lr_2_Seg)
 lr_3_Seg=lm(arrival_time ~ temperature + Age + Elevation, data=third_segment)
+summary(lr_3_Seg)
 lr_4_Seg=lm(arrival_time ~ temperature + Age + Elevation, data=fourth_segment)
+summary(lr_4_Seg)
 lr_5_Seg=lm(arrival_time ~ temperature + Age + Elevation, data=fifth_segment)
+summary(lr_5_Seg)
 lr_6_Seg=lm(arrival_time ~ temperature + Age + Elevation, data=sixth_segment)
+summary(lr_6_Seg)
 lr_7_Seg=lm(arrival_time ~ temperature + Age + Elevation, data=seventh_segment)
+summary(lr_7_Seg)
 lr_8_Seg=lm(arrival_time ~ temperature + Age + Elevation, data=eigth_segment)
-lr_9_Seg=lm(arrival_time ~ temperature + Age + Elevation, data=ninth_segment)
+
+
